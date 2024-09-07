56508e94ef42e314625d8b9956c6f6666861e2a1
Edwin Chigne <edwin4chigne@gmail.com>
2024-09-07 16:55:38 -0500

    Upload de los Media Queries para Smartphone y Tablet

--- js/script.js
+++ js/script.js
@@ -21,3 +21,60 @@ function updateCountdown() {
 }
 
 setInterval(updateCountdown, 1000);
+
+//MENU MOVIL
+document.addEventListener('DOMContentLoaded', function () {
+    // Selecciona los elementos necesarios
+    const navToggle = document.querySelector('.nav-toggle');
+    const navMobile = document.querySelector('.nav-mobile');
+    const navbarContainer = document.querySelector('.navbar-container');
+    const navExit = document.querySelector('.nav-exit i');
+
+    // Asegúrate de que los elementos existan antes de añadir los eventos
+    if (navToggle && navMobile && navbarContainer && navExit) {
+        // Función para mostrar el menú móvil y ocultar el contenedor de la barra de navegación
+        function showNavMobile() {
+            navMobile.style.display = 'block'; 
+            navMobile.classList.remove('hidden');
+            navbarContainer.classList.add('hidden');
+            document.body.style.overflow = 'hidden'; // Evita el desplazamiento del fondo
+        }
+
+        // Función para ocultar el menú móvil y mostrar el contenedor de la barra de navegación
+        function hideNavMobile() {
+            navMobile.style.display = 'none'; 
+            navMobile.classList.add('hidden');
+            navbarContainer.classList.remove('hidden');
+            document.body.style.overflow = ''; // Restaura el desplazamiento del fondo
+        }
+
+        // Agrega el evento de clic al ícono del menú móvil
+        navToggle.addEventListener('click', showNavMobile);
+
+        // Agrega el evento de clic al ícono de cerrar en el menú móvil
+        navExit.addEventListener('click', hideNavMobile);
+    }
+});
+
+//CARROUSEL DE SPONSOR
+document.addEventListener('DOMContentLoaded', function() {
+    const sponsors = document.querySelectorAll('.sponsor');
+    let currentIndex = 0;
+    const totalSponsors = sponsors.length;
+
+    // Función para mostrar la siguiente imagen
+    function showNextSponsor() {
+      // Ocultar la imagen actual
+      sponsors[currentIndex].classList.remove('active');
+
+      // Calcular el siguiente índice
+      currentIndex = (currentIndex + 1) % totalSponsors;
+
+      // Mostrar la siguiente imagen
+      sponsors[currentIndex].classList.add('active');
+    }
+
+    // Iniciar el carrusel
+    sponsors[currentIndex].classList.add('active'); // Mostrar la primera imagen
+    setInterval(showNextSponsor, 2000); // Cambiar cada 1 segundo
+  });
